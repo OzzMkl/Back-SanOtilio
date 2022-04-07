17f481e51510e7c911805c8920900afa1e3c6471
Oziel Pacheco <latikakm@gmail.com>
2022-04-07 18:48:06 -0500

    nada interesante

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Cliente;
 
 class ClienteController extends Controller
 {
