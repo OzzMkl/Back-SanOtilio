2ec4c2dbe44d5ad0fda690dd6b8953da815be3e0
Oziel Pacheco <latikakm@gmail.com>
2022-04-13 15:32:35 -0500

    api para agregar direccion del cliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -39,27 +39,26 @@ class ClienteController extends Controller
         $json = $request -> input('json',null);//recogemos los datos enviados por post en formato json
         $params = json_decode($json);
         $params_array = json_decode($json,true);
-
         if(!empty($params) && !empty($params_array)){
-            //$params_array = array_map('trim',$params_array);
+            $params_array = array_map('trim',$params_array);
 
-            $validate = Validator::make($params_array, [
-                'nombre'       => 'required',
-                'rfc'    => 'required',
-                'correo'      => 'required',
-                'credito'   => 'required',
-                'idStatus'   => 'required',
-                'idTipo'   => 'required'
-            ]);
+             $validate = Validator::make($params_array, [
+                 'nombre'       => 'required',
+                 'rfc'    => 'required',
+                 'correo'      => 'required',
+                 'credito'   => 'required',
+                 'idStatus'   => 'required',
+                 'idTipo'   => 'required'
+             ]);
 
-            if($validate->fails()){
-                $data = array(
-                    'status'    => 'error',
-                    'code'      => 404,
-                    'message'   => 'Fallo la validacion de los datos del cliente',
-                    'errors'    => $validate->errors()
-                );
-            }else{
+             if($validate->fails()){
+                 $data = array(
+                     'status'    => 'error',
+                     'code'      => 404,
+                     'message'   => 'Fallo la validacion de los datos del cliente',
+                     'errors'    => $validate->errors()
+                 );
+             }else{
                 try{
                     DB::beginTransaction();
                     $Cliente = new Cliente();
@@ -76,24 +75,6 @@ class ClienteController extends Controller
                     $Cliente->idTipo = $params_array['idTipo'];
                     $Cliente->save();
                     
-                    //consuktamos el ultimo insertado
-                    //$Cliente = Cliente::latest('idCliente')->first();
-//
-                    //$cdireccion = new Cdireccion();
-                    //$cdireccion->idCliente = $Cliente->idCliente;
-                    //$cdireccion->pais = $params_array[''];
-                    //$cdireccion->estado = $params_array[''];
-                    //$cdireccion->ciudad = $params_array[''];
-                    //$cdireccion->colonia = $params_array[''];
-                    //$cdireccion->calle = $params_array[''];
-                    //$cdireccion->numExt = $params_array[''];
-                    //$cdireccion->numInt = $params_array[''];
-                    //$cdireccion->cp = $params_array[''];
-                    //$cdireccion->referencia = $params_array[''];
-                    //$cdireccion->telefono = $params_array[''];
-                    //$cdireccion->idZona = $params_array[''];
-                    //$cdireccion->save();
-
                     DB::commit();
 
                     $data = array(
@@ -106,7 +87,7 @@ class ClienteController extends Controller
                     $data = array(
                         'code'      => 400,
                         'status'    => 'Error',
-                        'message'   =>  'Fallo al crear la orden compra Rollback!',
+                        'message'   =>  'Algo salio mal rollbak',
                         'error' => $e
                     );
                 }
@@ -122,5 +103,60 @@ class ClienteController extends Controller
         }
         return response()->json($data, $data['code']);
     }
+    public function registerCdireccion(Request $request){
+        $json = $request -> input('json',null);
+        $params_array = json_decode($json,true);
+        if(!empty($params_array)){
+            $params_array = array_map('trim',$params_array);
+
+             $validate = Validator::make($params_array, [
+                 'pais'       => 'required',
+                 'estado'    => 'required',
+                 'ciudad'      => 'required',
+                 'colonia'   => 'required',
+                 'calle'   => 'required',
+                 'numExt'   => 'required',
+                 'cp'   => 'required',
+                 'referencia'   => 'required',
+                 'telefono'   => 'required',
+                 'idZona'   => 'required'
+             ]);
+
+             if($validate->fails()){
+                 $data = array(
+                     'status'    => 'error',
+                     'code'      => 404,
+                     'message'   => 'Fallo la validacion de los datos del cliente',
+                     'errors'    => $validate->errors()
+                 );
+             }else{
+                  //consuktamos el ultimo insertado
+                    $Cliente = Cliente::latest('idCliente')->first();
+                    $cdireccion = new Cdireccion();
+                    $cdireccion->idCliente = $Cliente->idCliente;
+                    $cdireccion->pais = $params_array[''];
+                    $cdireccion->estado = $params_array[''];
+                    $cdireccion->ciudad = $params_array[''];
+                    $cdireccion->colonia = $params_array[''];
+                    $cdireccion->calle = $params_array[''];
+                    $cdireccion->numExt = $params_array[''];
+                    $cdireccion->numInt = $params_array[''];
+                    $cdireccion->cp = $params_array[''];
+                    $cdireccion->referencia = $params_array[''];
+                    $cdireccion->telefono = $params_array[''];
+                    $cdireccion->idZona = $params_array[''];
+                    $cdireccion->save();
+
+             }
+        }else{
+            $data = array(
+                'code'      =>  400,
+                'status'    => 'Error!',
+                'message'   =>  'json vacio'
+            );
+        }
+        return response()->json($data, $data['code']);
+    }
 }
 
+/** */
