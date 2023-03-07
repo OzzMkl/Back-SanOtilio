6dabbc458b760203362f53622f577e8888dc4dd9
oziel pacheco <ozielpacheco.m@gmail.com>
2023-03-07 02:28:42 -0600

    Creacion de modelo para la tabla historialproductos_medidas

---
+++ app/models/historialproductos_medidas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class historialproductos_medidas extends Model
+{
+    protected $table = 'historialproductos_medidas';
+    protected $fillable = [
+        'idProductoMedida',
+        'idProducto',
+        'idMedida',
+        'unidad',
+        'precioCompra',
+        'porcentaje1','precio1',
+        'porcentaje2','precio2',
+        'porcentaje3','precio3',
+        'porcentaje4','precio4',
+        'porcentaje5','precio5',
+    ];
+}
