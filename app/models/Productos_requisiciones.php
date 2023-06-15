c142233a73628c0ebdabc6e358ee6fad502d17f3
Oziel Pacheco <ozielpacheco.m@gmail.com>
2023-06-15 09:47:28 -0600

    Alta de modelo Francisco
    Co-authored-by: Ik-hor <Ik-hor@users.noreply.github.com>

--- app/models/Productos_requisiciones.php
+++ app/models/Productos_requisiciones.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Productos_requisiciones extends Model
 {
-    //
+    protected $table = 'productos_requisiciones';
+    protected $fillable = [
+        'idReq','idProducto','idProdMedida','cantidad','igualMedidaMenor'
+    ];
 }
