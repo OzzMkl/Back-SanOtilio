3f71bdd457ecb96028d4fa31993c6aaa2fb59091
Oziel Pacheco <ozielpacheco.m@gmail.com>
2023-09-19 17:23:30 -0600

    Creacion de modelos de traspasos

--- app/models/traspasoe.php
+++ app/models/traspasoe.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class traspasoe extends Model
 {
-    //
+    protected $table = 'traspasoe';
+    protected $primaryKey = 'idTraspasoE';
+    protected  $fillable = [
+        'folioD',
+        'sucursalE',
+        'sucursalR',
+        'idEnvio',
+        'idEmpleado',
+        'idStatus',
+        'observaciones'
+    ];
+    // public function sucursalE()
+    // {
+    //     return $this->belongsTo(Sucursal::class, 'sucursalE', 'idSuc')->select('nombre as sucursalEN');
+    // }
+
+    // public function sucursalR()
+    // {
+    //     return $this->belongsTo(Sucursal::class, 'sucursalR', 'idSuc')->select('nombre as sucursalRN');
+    // }
+
+    // public function empleado()
+    // {
+    //     return $this->belongsTo(\App\Empleado::class, 'idEmpleado', 'idEmpleado');
+    // }
+
+    // public function status()
+    // {
+    //     return $this->belongsTo(Statuss::class, 'idStatus', 'idStatus');
+    // }
 }
--- app/models/traspasor.php
+++ app/models/traspasor.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class traspasor extends Model
 {
-    //
+    protected $table = 'traspasor';
+    protected $primaryKey = 'idTraspasoR';
+    protected  $fillable = [
+        'folioO',
+        'sucursalE',
+        'sucursalR',
+        'idEnvio',
+        'idEmpleado',
+        'idStatus',
+        'observaciones'
+    ];
+    // public function sucursalE()
+    // {
+    //     return $this->belongsTo(Sucursal::class, 'sucursalE', 'idSuc');
+    // }
+
+    // public function sucursalR()
+    // {
+    //     return $this->belongsTo(Sucursal::class, 'sucursalR', 'idSuc');
+    // }
+
+    // public function empleado()
+    // {
+    //     return $this->belongsTo(\App\Empleado::class, 'idEmpleado', 'idEmpleado');
+    // }
+
+    // public function status()
+    // {
+    //     return $this->belongsTo(Statuss::class, 'idStatus', 'idStatus');
+    // }
 }
