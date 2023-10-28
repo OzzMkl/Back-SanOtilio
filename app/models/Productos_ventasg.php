787ab5da4f9efb940e8f3bf91adb59d2fd884a47
Oziel Pacheco <ozielpacheco.m@gmail.com>
2023-10-27 20:50:25 -0600

    Se agrega funcion global de insercion en productosVentasg

--- app/models/Productos_ventasg.php
+++ app/models/Productos_ventasg.php
@@ -8,8 +8,33 @@ class Productos_ventasg extends Model
 {
     protected $table = 'productos_ventasg';
     protected $fillable = [
-        'idVenta','idProducto','descripcion',
-        'idProdMedida','cantidad','precio',
-        'descuento','total','igualMedidaMenor'
+        'idVenta',
+        'idProducto',
+        'descripcion',
+        'idProdMedida',
+        'cantidad',
+        'precio',
+        'descuento',
+        'total',
+        'igualMedidaMenor'
     ];
+
+    public static function insertProductoVentasg($idVenta,$producto,$medidaMenor){
+        $productos_ventasg = new self([
+            'idVenta' => $idVenta,
+            'idProducto' => $producto['idProducto'],
+            'descripcion' => $producto['descripcion'],
+            'idProdMedida' => $producto['idProdMedida'],
+            'cantidad' => $producto['cantidad'],
+            'precio' => $producto['precio'],
+            'total' => $producto['subtotal'],
+            'igualMedidaMenor' => $medidaMenor,
+        ]);
+        if(isset($paramdata['descuento'])){
+            $productos_ventasg->descuento = $producto['descuento'];
+        }
+        
+        //guardamos el producto
+        $productos_ventasg->save();
+    }
 }
