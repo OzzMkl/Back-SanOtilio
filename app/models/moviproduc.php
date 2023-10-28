0194c278925c3acb802d2ff223e9208d95a5ec5b
Oziel Pacheco <ozielpacheco.m@gmail.com>
2023-10-27 20:49:57 -0600

    Se agrega funcion global de insercion en moviproduc

--- app/models/moviproduc.php
+++ app/models/moviproduc.php
@@ -9,8 +9,31 @@ class moviproduc extends Model
     protected $table = 'moviproduc';
     protected $primaryKey = 'idMovimiento';
     protected $fillable = [
-        'idProducto','claveEx','accion','folioAccion',
-        'cantidad','stockanterior','stockactualizado',
+        'idProducto',
+        'claveEx',
+        'accion',
+        'folioAccion',
+        'cantidad',
+        'stockanterior',
+        'stockactualizado',
         'idUsuario','pc'
     ];
+
+    public static function insertMoviproduc($producto,$accion,$folioAccion,$medidaMenor,$stockAnterior,$stockActualizado,$idEmpleado){
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        $moviproduc = new self([
+            'idProducto' => $producto['idProducto'],
+            'claveEx' => $producto['claveEx'],
+            'accion' => $accion,
+            'folioAccion' => $folioAccion,
+            'cantidad' => $medidaMenor,
+            'stockanterior' => $stockAnterior,
+            'stockactualizado' => $stockActualizado,
+            'idUsuario' => $idEmpleado,
+            'pc' => $ip,
+        ]);
+        
+        $moviproduc ->save();
+    }
 }
