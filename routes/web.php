b273b7f6b109129e45f14c998284531284f88259
Paco <pacosalomon6043@gmail.com>
2023-11-28 09:16:51 -0600

    Se retira método registerTraspasoExterno Y se deja updateTraspaso

--- routes/web.php
+++ routes/web.php
@@ -225,8 +225,7 @@ Route::get('/prueba', function(){
     Route::post('/api/traspasos/cancelarTraspaso','TraspasosController@cancelarTraspaso');
     Route::get('/api/traspasos/generatePDF/{idTraspaso}/{idempleado}/{tipoTraspaso}','TraspasosController@generatePDF');
     Route::get('/api/traspasos/showMejorado/{idTraspaso}/{tipoTraspaso}','TraspasosController@showMejorado');
-    Route::get('/api/traspasos/updateTraspaso','TraspasosController@updateTraspaso');
-    Route::get('/api/traspasos/registerTraspasoExterno/{idTraspasoE}/{sucursalR}','TraspasosController@registerTraspasoExterno');
+    Route::post('/api/traspasos/updateTraspaso','TraspasosController@updateTraspaso');
 
 
 
