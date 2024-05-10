fcf62c183065660ad59c4b9b45f3b193779cad80
Oziel Pacheco <ozielpacheco.m@gmail.com>
2024-05-09 22:44:31 -0600

    Cambio de zona horaria de Mexico_city a Guatemala - Esto es una correcion temporal ya que el de Mexico tiene horario de verano

--- config/app.php
+++ config/app.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'timezone' => 'America/Mexico_City',
+    'timezone' => 'America/Guatemala',
 
     /*
     |--------------------------------------------------------------------------
